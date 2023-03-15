d0028717efea57639e269ffec48b6dffb21785ce
Alik del Risco <alik.delrisco@rideco.com>
2023-03-15 10:50:26 -0400

    v3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
 
 import App from './App.vue'
 import router from './router'
